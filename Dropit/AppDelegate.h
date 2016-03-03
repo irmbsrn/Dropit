3ea9a6a00fda4db4be961ebcd39c69fb92aacb75
GBGYP-9 <gbgyp-9@gbgyp-9-imac.local>
2016-03-03 20:02:33 +0200

    Initial Commit

---
+++ Dropit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dropit
+//
+//  Created by GBGYP-9 on 3.03.2016.
+//  Copyright © 2016 GBGYP-9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dropit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dropit
+//
+//  Created by GBGYP-9 on 3.03.2016.
+//  Copyright © 2016 GBGYP-9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
